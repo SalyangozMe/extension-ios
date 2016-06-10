b339392ece95b8df20a31c0036177053345a0abd
Said <saidozcn@gmail.com>
2016-06-10 17:04:15 +0300

    adds user messages after sharing

--- SalyangozShareExtension/ShareViewController.swift
+++ SalyangozShareExtension/ShareViewController.swift
@@ -14,9 +14,14 @@ import MobileCoreServices
 class ShareViewController: UIViewController {
     
     
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var messageLabel: UILabel!
         
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.containerView.layer.cornerRadius = 20
+        self.containerView.layer.masksToBounds = true
         
         guard let context = self.extensionContext else { return }
         guard let inputItems = context.inputItems as? [NSExtensionItem] else { return }
@@ -47,8 +52,28 @@ class ShareViewController: UIViewController {
     }
     
     func sharePage(url: String, title: String){
-        SalyangozAPI.sharedAPI.shareToSalyangoz(url, title: title) { (response, error) in
-            self.extensionContext?.completeRequestReturningItems(nil, completionHandler: nil)
+        if DataManager.sharedManager.isLoggedIn(){
+            SalyangozAPI.sharedAPI.shareToSalyangoz(url, title: title) {  [unowned self] (response, error) in
+                if let _ = response{
+                    self.messageLabel.text = NSLocalizedString("Shared!", comment: "")
+                }else{
+                    self.messageLabel.text = NSLocalizedString("Error!", comment: "")
+                }
+                self.complete()
+            }
+        }else{
+            self.messageLabel.text = NSLocalizedString("Please login first!", comment: "")
+            self.complete()
+        }
+    }
+    
+    func complete(){
+        let delayInSeconds = 1.5
+        let delay = dispatch_time(DISPATCH_TIME_NOW, Int64(delayInSeconds * Double(NSEC_PER_SEC)))
+        dispatch_after(delay, dispatch_get_main_queue()) {
+            dispatch_async(dispatch_get_main_queue(), {
+                self.extensionContext?.completeRequestReturningItems(nil, completionHandler: nil)
+            })
         }
     }
 
