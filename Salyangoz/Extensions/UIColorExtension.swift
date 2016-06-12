fc9139f8a5e7a8938ede7da25349735cf757d02a
Said <saidozcn@gmail.com>
2016-06-12 22:01:06 +0300

    adds UIColor and UIViewController extensions

---
+++ Salyangoz/Extensions/UIColorExtension.swift
@@ -0,0 +1,20 @@
+//
+//  UIColorExtension.swift
+//  Salyangoz
+//
+//  Created by Muhammed Said Özcan on 11/06/16.
+//  Copyright © 2016 Tower Labs. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+extension UIColor{
+    public static func applicationNavigationTitleColor() -> UIColor{
+        return UIColor(red: 0.04, green: 0.04, blue: 0.04, alpha: 1)
+    }
+    
+    public static func applicationTabBarBorderTopColor() -> UIColor{
+        return UIColor(red:0.70, green:0.70, blue:0.70, alpha:1.00)
+    }
+}
---
+++ Salyangoz/Extensions/UIViewControllerExtension.swift
@@ -0,0 +1,30 @@
+//
+//  UIViewController.swift
+//  Salyangoz
+//
+//  Created by Muhammed Said Özcan on 11/06/16.
+//  Copyright © 2016 Tower Labs. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+extension UIViewController {
+    
+    func alert(title: String = "", message: String, action: UIAlertAction){
+        _alert(title, message: message, firstAction: action, secondAction: nil)
+    }
+    
+    func alert(title: String = "", message: String, firstAction: UIAlertAction, secondAction: UIAlertAction){
+        _alert(title, message: message, firstAction: firstAction, secondAction: secondAction)
+    }
+    
+    private func _alert(title: String = "", message: String, firstAction: UIAlertAction, secondAction: UIAlertAction?){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        alertController.addAction(firstAction)
+        if let secondAction = secondAction{
+            alertController.addAction(secondAction)
+        }
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+}
