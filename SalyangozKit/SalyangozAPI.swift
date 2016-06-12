e26d9f194abe698b0969b94b53fddbbcaa00d595
Said <saidozcn@gmail.com>
2016-06-12 22:00:42 +0300

    adds recent endpoint and updates login endpoint

--- SalyangozKit/SalyangozAPI.swift
+++ SalyangozKit/SalyangozAPI.swift
@@ -11,27 +11,41 @@ import Alamofire
 import AlamofireObjectMapper
 
 enum Router: URLRequestConvertible{
-    static let baseURL = NSURL(string: "http://salyangoz.me/api/v1")!
+    static let baseURL = NSURL(string: "https://salyangoz.me")!
+    static let baseServiceURL = NSURL(string: "https://salyangoz.me/api/v1/mobile")!
     
+    case Recent
     case Home(Int, String)
     case Login(String, String)
     case ShareToSalyangoz(Int, String, String, String)
     
-    var URL: NSURL { return Router.baseURL.URLByAppendingPathComponent(route.path) }
+    var URL: NSURL {
+        switch self{
+        case .Recent:
+            return Router.baseURL.URLByAppendingPathComponent(route.path)
+        default:
+            return Router.baseServiceURL.URLByAppendingPathComponent(route.path)
+        }
+        
+    }
     
     var route: (path: String, parameters: [String: AnyObject]?) {
         switch self {
         case .Login(let authToken, let authTokenSecret):
             return ("/login", ["token":authToken, "secret":authTokenSecret])
+        case .Recent:
+            return ("/recent.json", nil)
         case .Home(let userId, let userToken):
-            return ("/home", ["id":userId, "token":userToken])
+            return ("/posts/home", ["id":userId, "token":userToken])
         case .ShareToSalyangoz(let userId, let token, let url, let title):
-            return ("/post/add", ["id":userId, "token": "\(token)", "url":"\(url)", "title":"\(title)"])
+            return ("/posts", ["id":userId, "token": "\(token)", "url":"\(url)", "title":"\(title)"])
         }
     }
     
     var method: Alamofire.Method{
         switch self{
+        case .Recent:
+            return .GET
         case .Home:
             return .GET
         case .Login:
@@ -44,6 +58,7 @@ enum Router: URLRequestConvertible{
     var URLRequest: NSMutableURLRequest {
         let httpRequest = NSMutableURLRequest(URL: URL)
         httpRequest.HTTPMethod = method.rawValue
+        httpRequest.setValue("application/json", forHTTPHeaderField: "Accept")
         return Alamofire.ParameterEncoding.JSON.encode(httpRequest, parameters: route.parameters).0
     }
 }
@@ -51,35 +66,63 @@ enum Router: URLRequestConvertible{
 public class SalyangozAPI{
     public static let sharedAPI = SalyangozAPI()
     
-    public func login(authToken:String, authTokenSecret: String, completion:(response: User?, error: NSError?) -> Void){
-        Alamofire.request(Router.Login(authToken, authTokenSecret)).responseJSON { (response: Response<AnyObject, NSError>) in
-            if let jsonResult = response.result.value as? [String: AnyObject]{
-                
-                guard let status = jsonResult["status"] as? Bool else { return }
-                if status{
-                    guard let token = jsonResult["token"] as? String else { return }
-                    guard let userId = jsonResult["id"] as? Int else { return }
-                    
-                    let user = User(userId: userId, token: token)
+    public func login(authToken:String, authTokenSecret: String, completion:(success: Bool) -> Void){
+        Alamofire.request(Router.Login(authToken, authTokenSecret)).responseObject { (response:Response<LoginResponse, NSError>) in
+            switch response.result{
+            case .Success:
+                if let loginResponse = response.result.value{
+                    let user = SessionUser(userId: loginResponse.userId, token: loginResponse.token)
                     DataManager.sharedManager.createSession(user)
-                    completion(response: user, error: response.result.error)
-                }else{
-                    completion(response: nil, error: response.result.error)
+                    dispatch_async(dispatch_get_main_queue(), {
+                        completion(success: true)
+                    })
                 }
+            case .Failure(let error):
+                dispatch_async(dispatch_get_main_queue(), {
+                    completion(success: false)
+                })
+                print(error.localizedDescription)
             }
         }
     }
     
-    public func shareToSalyangoz(url:String, title:String, completion:(response: String?, error: NSError?)->Void){
+    public func shareToSalyangoz(url:String, title:String, completion:(success: Bool)->Void){
         guard let session = DataManager.sharedManager.getSession() else { return }
         let userId = session.userId
         let token  = session.token
         
-        
-        Alamofire.request(Router.ShareToSalyangoz(userId, token, url, title)).responseString { (response:Response<String, NSError>) in
-            print(response)
-            completion(response: response.result.value, error: response.result.error)
+        Alamofire.request(Router.ShareToSalyangoz(userId, token, url, title)).responseJSON(completionHandler: { (response: Response<AnyObject, NSError>) in
+            dispatch_async(dispatch_get_main_queue(), { 
+                switch response.result{
+                case .Success:
+                    completion(success:true)
+                case .Failure(_):
+                    completion(success:false)
+                }
+            })
+        })
+    }
+    
+    public func getRecentFeed(completion:(feed:[Post]?, error:NSError?)->Void){
+        Alamofire.request(Router.Recent).responseArray(keyPath:"posts"){ (response: Response<[Post], NSError>) in
+            
+            switch response.result{
+            case .Success:
+                if let feed = response.result.value{
+                    dispatch_async(dispatch_get_main_queue(), {
+                        completion(feed: feed, error: nil)
+                    })
+                }else{
+                    dispatch_async(dispatch_get_main_queue(), {
+                        completion(feed: nil, error: nil)
+                    })
+                }
+            case .Failure(let error):
+                print(error.localizedDescription)
+                dispatch_async(dispatch_get_main_queue(), {
+                    completion(feed: nil, error: error)
+                })
+            }
         }
     }
-
 }
