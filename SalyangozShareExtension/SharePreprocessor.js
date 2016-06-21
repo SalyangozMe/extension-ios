b6e92bfc278c3cc19b1cf0489f71f429e6fa0b33
Said <saidozcn@gmail.com>
2016-06-21 17:00:53 +0300

    share extension updates to support sharing from any application

--- SalyangozShareExtension/SharePreprocessor.js
+++ SalyangozShareExtension/SharePreprocessor.js
@@ -3,7 +3,6 @@ var MyPreprocessor = function() {};
 MyPreprocessor.prototype = {
 run: function(arguments) {
     arguments.completionFunction({"URL": document.URL,
-                                 "pageSource": document.documentElement.outerHTML,
                                  "title": document.title,
                                  "selection": window.getSelection().toString()});
 }
--- SalyangozShareExtension/ShareViewController.swift
+++ SalyangozShareExtension/ShareViewController.swift
@@ -16,6 +16,17 @@ class ShareViewController: UIViewController {
     
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var messageLabel: UILabel!
+    
+    var urlAttachmentProvider: NSItemProvider?
+    
+    override func beginRequestWithExtensionContext(context: NSExtensionContext) {
+        super.beginRequestWithExtensionContext(context)
+        if let input = context.inputItems as? [NSExtensionItem]{
+            if let attachments = input[0].attachments{
+                urlAttachmentProvider = attachments[0] as? NSItemProvider
+            }
+        }
+    }
         
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,35 +34,36 @@ class ShareViewController: UIViewController {
         self.containerView.layer.cornerRadius = 20
         self.containerView.layer.masksToBounds = true
         
-        guard let context = self.extensionContext else { return }
-        guard let inputItems = context.inputItems as? [NSExtensionItem] else { return }
-        
-        for item: NSExtensionItem in inputItems{
-            guard let attachments = item.attachments as? [NSItemProvider] else { break }
-            
-            for attachment: NSItemProvider in attachments{
-                if attachment.hasItemConformingToTypeIdentifier(kUTTypePropertyList as String) {
-                    
-                    let completionHandler: NSItemProviderCompletionHandler = { (result: NSSecureCoding?, error: NSError!) in
-                        guard let resultsDict = result as? NSDictionary else { return }
-                        guard let preprocessingResults = resultsDict[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
-                        guard let url = preprocessingResults["URL"] as? String else { return }
-                        guard let title = preprocessingResults["title"] as? String else { return }
-                        
-                        self.sharePage(title, url: url)
+        if let provider = urlAttachmentProvider{
+            if provider.hasItemConformingToTypeIdentifier(kUTTypeURL as String) ||
+                provider.hasItemConformingToTypeIdentifier(kUTTypePropertyList as String) {
+                
+                let completionHandler: NSItemProviderCompletionHandler = { (result: NSSecureCoding?, error: NSError!) in
+                    if error == nil {
+                        if let url = result as? NSURL{
+                            self.sharePage("", url: url.absoluteString)
+                        }else{
+                            self.complete(0)
+                        }
+                    }else{
+                        self.complete(0)
                     }
-                    
-                    attachment.loadItemForTypeIdentifier(kUTTypePropertyList as String,
-                                                         options: nil,
-                                                         completionHandler: completionHandler);
                 }
+                
+                provider.loadItemForTypeIdentifier(kUTTypeURL as String,
+                                                     options: nil,
+                                                     completionHandler: completionHandler)
+            }else{
+                self.complete(0)
             }
+        }else{
+            self.complete(0)
         }
-        
-        
     }
     
-    func sharePage(title: String, url: String){
+    func sharePage(title: String?, url: String?){
+        guard let title = title else { return }
+        guard let url = url else { return }
         if DataManager.sharedManager.isLoggedIn(){
             let post = Post(title: title, url: NSURL(string: url)!)
             SalyangozAPI.sharedAPI.sharePost(post, completion: { [unowned self] (success: Bool) in
@@ -60,11 +72,11 @@ class ShareViewController: UIViewController {
                 }else{
                     self.setMessageLabelText(NSLocalizedString("Error!", comment: ""))
                 }
-                self.complete()
+                self.complete(1)
             })
         }else{
             self.setMessageLabelText(NSLocalizedString("Please login first!", comment: ""))
-            self.complete()
+            self.complete(1)
         }
     }
     
@@ -74,9 +86,8 @@ class ShareViewController: UIViewController {
         })
     }
     
-    func complete(){
-        let delayInSeconds = 1.5
-        let delay = dispatch_time(DISPATCH_TIME_NOW, Int64(delayInSeconds * Double(NSEC_PER_SEC)))
+    func complete(delay: Double = 1.5){
+        let delay = dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC)))
         dispatch_after(delay, dispatch_get_main_queue()) {
             self.extensionContext?.completeRequestReturningItems(nil, completionHandler: nil)
         }
